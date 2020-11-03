39eb9f413ba2973c3832106c27ce0615844f0322
Renê Cardozo <rene.epcrdz@gmail.com>
2020-11-03 03:58:17 -0300

    Ciclistas andam na pista

--- pista.c
+++ pista.c
@@ -2,6 +2,8 @@
 #include "aleatorio.h"
 #include <math.h>
 
+#define LOG_FD stdout
+
 int d;
 int n;
 pthread_mutex_t mutex_n;
@@ -14,6 +16,7 @@ int intervalo = 60; // Multiplicado pela velocidade, obtem-se a distância perco
 int ultimas = 0; // Variável booleana que indica se está nas últimas duas voltas
 pthread_mutex_t mutex_ultimas;
 
+int* ids;
 linha* pista;
 rank* ranking;
 pthread_barrier_t* barreiras;
@@ -23,6 +26,37 @@ void uso() {
     fprintf(stderr, "\t./ep2 <tamanho-da-pista> <numero-de-ciclistas>\n");
     exit(EXIT_FAILURE);
 }
+
+void cria_barreiras() {
+    pthread_barrier_init(&largada, NULL, n+1);
+
+    // São criadas duas barreiras, as quais serão alternadas a cada 
+    // turno da simulação. De modo que, enquanto as n threads ciclistas
+    // restantes esperam uma barreira, a do outro turno é criada.
+    pthread_barrier_init(&barr[0], NULL, n+1);
+    pthread_barrier_init(&barr[1], NULL, n+1);
+}
+
+void cria_pista(pthread_t id_threads[]) {
+    // Inicia pista
+    pista = calloc(d, sizeof(linha));
+    for(int i = 0; i < d; i++) {
+        for(int j = 0; j < NUM_FAIXAS; j++) {
+            pthread_mutex_init(&pista[i].mutex_pos[j], NULL);
+            pista[i].pos[j] = -1;
+        }
+    }
+
+    // Distribui os ciclistas nas faixas 0, 2, 4, 6 e 8.
+    ids = malloc(n*sizeof(int));
+    for(int z = 0; z < n; z++) {
+        ids[z] = z;
+        pthread_create(&id_threads[z], NULL, (void*)&ciclista, &ids[z]);
+        pista[2*z/NUM_FAIXAS].pos[2*z%NUM_FAIXAS] = z;
+        pista[2*z/NUM_FAIXAS].n_ciclistas += 1;
+    }
+}
+
 
 void print_pista(linha* pista) {
     for(int i = 0; i < d; i++) {
@@ -34,7 +68,64 @@ void print_pista(linha* pista) {
     }
 }
 
-void* ciclista() {
+void atualiza_numero(int pista_atual, int prox_pista) {
+    pthread_mutex_lock(&pista[prox_pista].mutex_linha);
+    pthread_mutex_lock(&pista[pista_atual].mutex_linha);
+    pista[prox_pista].n_ciclistas++;
+    pista[pista_atual].n_ciclistas--;
+    pthread_mutex_unlock(&pista[prox_pista].mutex_linha);
+    pthread_mutex_unlock(&pista[pista_atual].mutex_linha);
+}
+
+int verifica_quebra() {
+    int quebrou;
+    pthread_mutex_lock(&mutex_n);
+    quebrou = decide_quebrou(n);
+    if(quebrou) {
+        fprintf(LOG_FD, "O ciclista quebrou\n");
+        //n -= 1;
+        fprintf(LOG_FD, "%d restantes\n", n);
+    }
+    pthread_mutex_unlock(&mutex_n);
+    return quebrou;
+}
+
+int atualiza_velocidade(int* vel) {
+    *vel = decide_velocidade(*vel, ultimas);
+    if(*vel == 8) return 1.0;
+    else if(*vel == 16) return 2.0;
+    else {
+        pthread_mutex_lock(&mutex_ultimas);
+        ultimas = 1;
+        pthread_mutex_unlock(&mutex_ultimas);
+        return 0.0;
+    }
+}
+
+void atualiza_posicao(int* pista_atual, int* prox_pista, int* faixa_atual, int id, int* metro_atual, int* pos_relativa) {
+    pthread_mutex_lock(&pista[*prox_pista].mutex_pos[*faixa_atual]);
+    pthread_mutex_lock(&pista[*pista_atual].mutex_pos[*faixa_atual]);
+    if(pista[*prox_pista].pos[*faixa_atual] == -1) {
+        pista[*prox_pista].pos[*faixa_atual] = id;
+        pista[*pista_atual].pos[*faixa_atual] = -1;
+
+        atualiza_numero(*pista_atual, *prox_pista);
+
+        pthread_mutex_unlock(&pista[*pista_atual].mutex_pos[*faixa_atual]);
+        pthread_mutex_unlock(&pista[*prox_pista].mutex_pos[*faixa_atual]);
+
+        *pista_atual = *prox_pista;
+        *prox_pista = *pista_atual - 1;
+        if(*prox_pista == -1) *prox_pista = d - 1;
+        *metro_atual += 1;
+        *pos_relativa = 0;
+    } else {
+        pthread_mutex_unlock(&pista[*pista_atual].mutex_pos[*faixa_atual]);
+        pthread_mutex_unlock(&pista[*prox_pista].mutex_pos[*faixa_atual]);
+    }
+}
+
+void* ciclista(int* id) {
     int eliminado = 0;
     int quebrou = 0;
     int vel = 8;
@@ -42,6 +133,10 @@ void* ciclista() {
     int metro_atual = 0;
     int pos_relativa = 0;
     double resto = 1.0;
+    int faixa_atual = 2*(*id)%NUM_FAIXAS;
+    int pista_atual = 2*(*id)/NUM_FAIXAS;
+    int prox_pista = pista_atual - 1;
+    if(prox_pista == -1) prox_pista = d - 1;
 
     pthread_barrier_wait(&largada);
     // Cada ciclista eliminado deverá decrementar a variável
@@ -52,36 +147,20 @@ void* ciclista() {
         if(turno == 0) {
 
             if(volta != 1 && metro_atual == 0) {
-                vel = decide_velocidade(vel, ultimas);
-                if(vel == 8) resto = 1.0;
-                else if(vel == 16) resto = 2.0;
-                else {
-                    pthread_mutex_lock(&mutex_ultimas);
-                    ultimas = 1;
-                    pthread_mutex_unlock(&mutex_ultimas);
-                }
+                resto = atualiza_velocidade(&vel);
             }
             if(metro_atual == 0 && volta % 6 == 0) {
-                pthread_mutex_lock(&mutex_n);
-                quebrou = decide_quebrou(n);
-                if(quebrou) {
-                    if(n != 0) {
-                        n -= 1;
-                    }
-                    fprintf(stderr, "O ciclista quebrou");
-                }
-                pthread_mutex_unlock(&mutex_n);
+                quebrou = verifica_quebra();
             }
 
             pos_relativa += vel*intervalo + ceil((resto*intervalo)/3);
             if(pos_relativa >= 1000.0) {
-                metro_atual += 1;
-                pos_relativa = 0;
+                atualiza_posicao(&pista_atual, &prox_pista, &faixa_atual, *id, &metro_atual, &pos_relativa);
             }
             if(metro_atual == d) {
                 volta += 1;
                 metro_atual = 0;
-                fprintf(stderr, "Thread: %ld Volta %d Velocidade: %d\n", pthread_self(), volta, vel);
+                fprintf(LOG_FD, "Thread: %3d Volta %d Velocidade: %d\n", *id, volta, vel);
             }
 
             //// Atualiza velocidade e estado, utilizando as funções aleatórias.
@@ -94,61 +173,38 @@ void* ciclista() {
             //}
             pthread_barrier_wait(&barr[0]);
             pthread_barrier_wait(&barr[0]);
-            if(quebrou) break;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////TURNO ÍMPAR//////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
         } else {
-            if(volta != 1) {
-                vel = decide_velocidade(vel, ultimas);
-                if(vel == 8) resto = 1.0;
-                else if(vel == 16) resto = 2.0;
-                else resto = 0;
-            }
 
+            if(volta != 1 && metro_atual == 0) {
+                resto = atualiza_velocidade(&vel);
+            }
             if(metro_atual == 0 && volta % 6 == 0) {
-                pthread_mutex_lock(&mutex_n);
-                quebrou = decide_quebrou(n);
-                if(quebrou) {
-                    if(n != 0) {
-                        n -= 1;
-                    }
-                    fprintf(stderr, "O ciclista quebrou");
-                }
-                pthread_mutex_unlock(&mutex_n);
+                quebrou = verifica_quebra();
             }
 
             pos_relativa += vel*intervalo + ceil((resto*intervalo)/3);
+
             if(pos_relativa >= 1000.0) {
-                metro_atual += 1;
-                pos_relativa = 0;
+                atualiza_posicao(&pista_atual, &prox_pista, &faixa_atual, *id, &metro_atual, &pos_relativa);
             }
+
             if(metro_atual == d) {
                 volta += 1;
                 metro_atual = 0;
-                fprintf(stderr, "Thread: %ld Volta %d Velocidade: %d\n", pthread_self(), volta, vel);
+                fprintf(LOG_FD, "Thread: %3d Volta %d Velocidade: %d\n", *id, volta, vel);
             }
 
-
-            // Atualiza velocidade e estado, utilizando as funções aleatórias.
-            //if(eliminado || quebrou) {
-            //    // Encontrar alguma forma de controlar a aleatoriedade da destruição
-            //    // das threads e de atualizar o ranking.
-            //    pthread_mutex_lock(&mutex_n);
-            //    n -= 1;
-            //    pthread_mutex_unlock(&mutex_n);
-            //}
             pthread_barrier_wait(&barr[1]);
             pthread_barrier_wait(&barr[1]);
-            if(quebrou) break;
         }
     }
     return NULL;
 }
-
 int main(int argc, char** argv) {
-
     if(argc != 3)
         uso();
         
@@ -160,39 +216,20 @@ int main(int argc, char** argv) {
     pthread_mutex_init(&mutex_n, NULL);
     pthread_mutex_init(&mutex_ultimas, NULL);
 
-    // Inicia pista
-    pista = calloc(d, sizeof(linha));
-    for(int i = 0; i < d; i++) {
-        for(int j = 0; j < NUM_FAIXAS; j++)
-            pthread_mutex_init(&pista[i].mutex_pos[j], NULL);
-    }
-
-    pthread_barrier_init(&largada, NULL, n+1);
-
-    // São criadas duas barreiras, as quais serão alternadas a cada 
-    // turno da simulação. De modo que, enquanto as n threads ciclistas
-    // restantes esperam uma barreira, a do outro turno é criada.
-    pthread_barrier_init(&barr[0], NULL, n+1);
-    pthread_barrier_init(&barr[1], NULL, n+1);
-
-    // Distribui os ciclistas nas faixas 0, 2, 4, 6 e 8.
+    cria_barreiras();
     pthread_t id_threads[n];
-    int ids[n];
-    for(int z = 0; z < n; z++) {
-        ids[z] = z + 1;
-        pthread_create(&id_threads[z], NULL, (void*)&ciclista, NULL);
-        pista[2*z/NUM_FAIXAS].pos[2*z%NUM_FAIXAS] = z + 1;
-        pista[2*z/NUM_FAIXAS].n_ciclistas += 1;
-    }
-    print_pista(pista);
+    cria_pista(id_threads);
+
+
 
     pthread_barrier_wait(&largada);
     while(1) {
         pthread_barrier_wait(&barr[turno]);
-        pthread_barrier_destroy(&barr[!turno]);
+        print_pista(pista);
         if(n == 0) exit(EXIT_SUCCESS);
         if(ultimas) intervalo = 20;
-        // Aqui será feito todo o processamento da main.
+
+        pthread_barrier_destroy(&barr[!turno]);
         pthread_barrier_init(&barr[!turno], NULL, n+1);
         turno = !turno;
         pthread_barrier_wait(&barr[!turno]);
--- pista.h
+++ pista.h
@@ -9,11 +9,23 @@
 
 #define NUM_FAIXAS 10
 
+void cria_pista(pthread_t id_threads[]);
+
+void atualiza_numero(int pista_atual, int prox_pista);
+
+int verifica_quebra();
+
+int atualiza_velocidade(int* vel);
+
+void atualiza_posicao(int* pista_atual, int* prox_pista, int* faixa_atual, int id, int* metro_atual, int* pos_relativa);
+
+void* ciclista(int* id);
+
 typedef struct linha_t {
     int pos[NUM_FAIXAS];        // Em cada linha há NUM_FAIXAS posições possíveis
     pthread_mutex_t mutex_pos[NUM_FAIXAS];
     int n_ciclistas;
-    pthread_mutex_t mutex_linha;    // Experimento
+    pthread_mutex_t mutex_linha;
 } linha;
 
 typedef struct rank_t {
